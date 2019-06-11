4c575b20704e558a84ca079149b9869981a24f6f
Felix <felixletsplay@outlook.de>
2019-06-11 19:45:31 +0200

    Add editSheet: Create a new Scheme, later used in NotenTool

---
+++ js/editSheet.js
@@ -0,0 +1,32 @@
+function validatePercentage(grade) {
+    let percentage = parseInt(document.getElementById("grade" + grade).value);
+    if (grade !== 15 && percentage !== 100) {
+        if (percentage > parseInt(document.getElementById("grade" + (grade + 1)).value)) {
+            document.getElementById("grade" + (grade + 1)).value = (percentage + 1) + "";
+            validatePercentage(grade + 1)
+        }
+    } else if (grade !== 15 && percentage === 100) {
+        alert("Percentage should not be 100 while grade is not 15")
+    }
+    if (grade !== 0 && percentage !== 0) {
+        if (percentage < parseInt(document.getElementById("grade" + (grade - 1)).value)) {
+            document.getElementById("grade" + (grade - 1)).value = (percentage - 1) + "";
+            validatePercentage(grade - 1)
+        }
+    } else if (grade !== 0 && percentage === 0) {
+        alert("Percentage should not be 0 while grade is not 0")
+    }
+}
+
+function downloadConfig() {
+    let link = document.createElement('a');
+    link.download = 'grades.ntt';
+    link.href = 'data:,';
+    for (let grade = 0; grade <= 15; grade++) {
+        let percentage = document.getElementById("grade" + grade).value;
+        link.href += grade + "," + percentage + (grade !== 15 ? "%0A" : "");
+    }
+    document.body.append(link);
+    link.click();
+    document.body.removeChild(link);
+}
